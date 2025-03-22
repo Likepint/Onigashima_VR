6f9482115bf0a1bce904287ab3e2b72aba5cefea
Likepint <jsuuu96@naver.com>
2025-03-23 02:47:06 +0900

    BuildComponent_V4

--- Source/Onigashima/PJS/Builds/CBuildMesh.cpp
+++ Source/Onigashima/PJS/Builds/CBuildMesh.cpp
@@ -1,1 +1,67 @@
 #include "PJS/Builds/CBuildMesh.h"
+#include "Global.h"
+#include "Components/BoxComponent.h"
+
+ACBuildMesh::ACBuildMesh()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CHelpers::CreateComponent<UStaticMeshComponent>(this, &StaticMesh, "StaticMesh");
+
+	//CHelpers::CreateComponent<UBoxComponent>(this, &Box1, "Box1", StaticMesh);
+	//CHelpers::CreateComponent<UBoxComponent>(this, &Box2, "Box2", StaticMesh);
+	//CHelpers::CreateComponent<UBoxComponent>(this, &Box3, "Box3", StaticMesh);
+	//CHelpers::CreateComponent<UBoxComponent>(this, &Box4, "Box4", StaticMesh);
+
+	for (int32 i = 0; i < 4; ++i)
+	{
+		FString name = FString::Printf(TEXT("Box%d"), i + 1);
+		UBoxComponent* box = CreateDefaultSubobject<UBoxComponent>(*name);
+
+		box->SetupAttachment(StaticMesh);
+
+		BoxComponents.Add(box);
+	}
+
+}
+
+void ACBuildMesh::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	CheckNull(StaticMesh);
+
+	FVector extent = StaticMesh->GetStaticMesh()->GetBounds().BoxExtent;
+
+	//Box1->SetRelativeLocation(FVector(extent.X * 2, 0, extent.Z));
+	//Box1->SetBoxExtent(FVector(extent));
+
+	//Box2->SetRelativeLocation(FVector(0, extent.Y * 2, extent.Z));
+	//Box2->SetBoxExtent(FVector(extent));
+
+	//Box3->SetRelativeLocation(FVector(-extent.X * 2, 0, extent.Z));
+	//Box3->SetBoxExtent(FVector(extent));
+
+	//Box4->SetRelativeLocation(FVector(0, -extent.Y * 2, extent.Z));
+	//Box4->SetBoxExtent(FVector(extent));
+
+	for (int i = 0; i < Axis.Num(); ++i)
+	{
+		BoxComponents[i]->SetRelativeLocation(FVector(extent.X * Axis[i].x, extent.Y * Axis[i].y, extent.Z));
+		BoxComponents[i]->SetBoxExtent(extent);
+
+	}
+
+}
+
+void ACBuildMesh::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ACBuildMesh::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/Onigashima/PJS/Builds/IBuild.cpp
@@ -0,0 +1,1 @@
+#include "PJS/Builds/IBuild.h"
